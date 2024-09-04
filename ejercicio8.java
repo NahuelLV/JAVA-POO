f771d72bbeb3cdcbadff8b44ed4600fd5ce755c1
NahuelLV <130933110+NahuelLV@users.noreply.github.com>
2024-09-04 12:27:07 -0300

    Create ejercicio8.java

---
+++ ejercicio8.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+
+
+public class POO {
+	  public static void main(String[] args) {
+		  Scanner leer = new Scanner(System.in);
+		  System.out.println("Ingrese su nombre: ");
+		  String nombre = leer.nextLine();
+		  
+		  System.out.println("Bienvenido" +" "+ nombre);
+		  
+		  }
+	        
+	   }
+
