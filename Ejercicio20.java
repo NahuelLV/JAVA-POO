da2f847e607c8a2a9651e1582b18cfda51c90fe5
NahuelLV <130933110+NahuelLV@users.noreply.github.com>
2024-09-22 21:27:54 -0300

    Update Ejercicio20.java

--- Ejercicio20.java
+++ Ejercicio20.java
@@ -4,19 +4,20 @@ public class app {
     public static void main(String[] args) {
  
         Scanner sc = new Scanner(System.in);
-        String contraseña="eureka";
+        String contrasena="1234";
  
         final int INTENTOS = 3;
         
-        boolean acierto=false;
+        boolean correcta=false;
  
         String password;
-        for (int i=0;i<INTENTOS && !acierto;i++){
-            System.out.println("Introduce una contraseña");
+        for (int i=0;i<INTENTOS && !correcta;i++){
+            System.out.print("Introduce una contrasena: ");
             password = sc.next();
-            if (password.equals(contraseña)){
-                System.out.println("Enhorabuena, acertaste");
-                acierto=true;
+ 
+            if (password.equals(contrasena)){
+                System.out.println("Bien ahi, le pegaste");
+                correcta=true;
             }
         }
     }
